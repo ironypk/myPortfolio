6de7549177779fb721f4b69c78b076272b67b19b
Konstantin <ironypk@gmail.com>
2019-07-20 19:30:18 +1200

    week_5 create

--- src/main.js
+++ src/main.js
@@ -8,3 +8,10 @@ import "./scripts/parallax";
 import "./scripts/slider";
 import "./scripts/rewiews";
 import "./scripts/menu";
+<<<<<<< HEAD
+=======
+import "./scripts/form";
+import "./scripts/contact";
+
+
+>>>>>>> 54ad1197c73018c3825fba3787aa2f70fea8c8ac
